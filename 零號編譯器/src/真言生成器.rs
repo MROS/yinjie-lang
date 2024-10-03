3a8e25707e241bd895a7a1dcf53614089bac1474
MROS <yc1043@gmail.com>
2024-10-03 22:21:01 +0800

    真言生成

--- 零號編譯器/src/真言生成器.rs
+++ 零號編譯器/src/真言生成器.rs
@@ -1,4 +1,5 @@
-use crate::剖析器::Ｏ語法樹;
+use crate::分詞器::Ｏ運算子;
+use crate::剖析器::{Ｏ二元運算, Ｏ句, Ｏ算式, Ｏ語法樹, Ｏ變數宣告};
 use std::{
     collections::HashSet,
     fs::File,
@@ -25,10 +26,12 @@ impl Ｏ真言生成器 {
         self.生成代碼段()
     }
 
+    // 好用函式
     fn 換行(&mut self) -> io::Result<()> {
         writeln!(self.真言檔, "")
     }
 
+    // 數據段
     fn 生成數據段(&mut self) -> io::Result<()> {
         writeln!(self.真言檔, ".section .data")?;
         self.生成變數標籤()
@@ -38,14 +41,89 @@ impl Ｏ真言生成器 {
             writeln!(self.真言檔, "{}:", 變數)?;
             // 初始值為 0
             // 其實初始值是多少不重要，通過符號檢查，代表每個變數使用前都會先賦值
-            writeln!(self.真言檔, "\t.word 0:")?;
+            writeln!(self.真言檔, "\t.quad 0")?;
         }
         self.換行()
     }
+
+    // 代碼段
     fn 生成代碼段(&mut self) -> io::Result<()> {
         writeln!(self.真言檔, ".section .text")?;
         writeln!(self.真言檔, ".global _start")?;
         self.換行()?;
+        writeln!(self.真言檔, "_start:")?;
+        let 語法樹 = &self.語法樹;
+
+        for 句 in &語法樹.句 {
+            match 句 {
+                Ｏ句::變數宣告(變數宣告) => Self::賦值(&mut self.真言檔, &變數宣告)?,
+                Ｏ句::算式(算式) => Self::計算(&mut self.真言檔, &算式)?,
+            }
+        }
+        writeln!(self.真言檔, "# 結束")?;
+        writeln!(self.真言檔, "\tli a7, 93")?; // RISCV Linux 中 exit 系統呼叫編號是 93
+        writeln!(self.真言檔, "\tmv a0, t0")?; // a0 = t0
+        writeln!(self.真言檔, "\tecall")?; // 執行系統呼叫 exit(t0)
         Ok(())
+    }
+
+    fn 賦值(真言檔: &mut File, 變數宣告: &Ｏ變數宣告) -> io::Result<()> {
+        Self::計算(真言檔, &變數宣告.算式)?;
+        writeln!(真言檔, "# 賦值給 {}", &變數宣告.變數名)?;
+        writeln!(真言檔, "\tsd t0, {}, s0", &變數宣告.變數名) // 存入變數所在記憶體
+    }
+
+    // 計算結束後，結果置於 t0
+    fn 計算(真言檔: &mut File, 算式: &Ｏ算式) -> io::Result<()> {
+        match 算式 {
+            Ｏ算式::二元運算(二元運算) => {
+                Self::計算(真言檔, 二元運算.左.as_ref())?;
+                Self::計算(真言檔, 二元運算.右.as_ref())?;
+                Self::二元運算(真言檔, &二元運算.運算子)
+            }
+            Ｏ算式::數字(數) => Self::數字入棧(真言檔, 數),
+            Ｏ算式::變數(變數) => Self::變數入棧(真言檔, 變數),
+        }
+    }
+    // 結束時，t0 = 數
+    fn 數字入棧(真言檔: &mut File, 數: &i64) -> io::Result<()> {
+        writeln!(真言檔, "# {} 入棧", 數)?;
+
+        writeln!(真言檔, "\taddi sp, sp, -8")?; // 增加棧 64 位元的空間
+        writeln!(真言檔, "\tli t0, {}", 數)?; // 將 t0 設為「數」
+        writeln!(真言檔, "\tsd t0, 0(sp)") // t0 放入棧頂
+    }
+    // 結束時，t0 = 變數
+    fn 變數入棧(真言檔: &mut File, 變數: &String) -> io::Result<()> {
+        writeln!(真言檔, "# 變數「{}」入棧", 變數)?;
+
+        writeln!(真言檔, "\taddi sp, sp, -8")?; // 增加棧 64 位元的空間
+        writeln!(真言檔, "\tld t0, {}", 變數)?; // t0 = *(i64*)變數
+        writeln!(真言檔, "\tsd t0, 0(sp)") // t0 放入棧頂
+    }
+    // 結束時，t0 = 二元運算結果
+    fn 二元運算(真言檔: &mut File, 運算子: &Ｏ運算子) -> io::Result<()> {
+        writeln!(真言檔, "# {:?}", 運算子)?;
+
+        writeln!(真言檔, "\tld t1, 0(sp)")?; // t1 = 棧頂
+        writeln!(真言檔, "\taddi sp, sp, 8")?; // 縮小棧
+        writeln!(真言檔, "\tld t0, 0(sp)")?; // t0 = 棧頂
+
+        match 運算子 {
+            Ｏ運算子::加 => {
+                writeln!(真言檔, "\tadd t0, t0, t1")?;
+            }
+            Ｏ運算子::減 => {
+                writeln!(真言檔, "\tsub t0, t0, t1")?;
+            }
+            Ｏ運算子::乘 => {
+                writeln!(真言檔, "\tmul t0, t0, t1")?;
+            }
+            Ｏ運算子::除 => {
+                writeln!(真言檔, "\tdiv t0, t0, t1")?;
+            }
+        }
+
+        writeln!(真言檔, "\tsd t0, 0(sp)") // t0 放入棧頂
     }
 }
