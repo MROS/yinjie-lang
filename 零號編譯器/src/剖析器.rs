7c161dce9ca646a54643b97000549196f02e726e
MROS <yc1043@gmail.com>
2024-10-12 11:32:46 +0800

    使用調度場算法區分運算子優先級

--- 零號編譯器/src/剖析器.rs
+++ 零號編譯器/src/剖析器.rs
@@ -38,6 +38,25 @@ pub struct Ｏ二元運算 {
     pub 右: Box<Ｏ算式>,
 }
 
+fn 優先級(運算子: &Ｏ運算子) -> u64 {
+    match 運算子 {
+        Ｏ運算子::乘 => 4,
+        Ｏ運算子::除 => 4,
+
+        Ｏ運算子::餘 => 3,
+
+        Ｏ運算子::加 => 2,
+        Ｏ運算子::減 => 2,
+
+        Ｏ運算子::等於 => 1,
+        Ｏ運算子::異於 => 1,
+        Ｏ運算子::小於 => 1,
+        Ｏ運算子::小於等於 => 1,
+        Ｏ運算子::大於 => 1,
+        Ｏ運算子::大於等於 => 1,
+    }
+}
+
 impl Ｏ剖析器 {
     pub fn new(詞流: VecDeque<Ｏ詞>) -> Self {
         Ｏ剖析器 { 詞組: 詞流 }
@@ -86,40 +105,61 @@ impl Ｏ剖析器 {
         let 游標 = self.消耗(游標, Ｏ詞::元)?;
         let 游標 = self.消耗(游標, Ｏ詞::音界)?;
         let (變數名, 游標) = self.剖析變數(游標)?;
-        let 游標 = self.消耗(游標, Ｏ詞::等號)?;
+        let 游標 = self.消耗(游標, Ｏ詞::賦值)?;
         let (算式, 游標) = self.剖析算式(游標)?;
 
         Some((Ｏ變數宣告 { 算式, 變數名 }, 游標))
     }
 
     fn 剖析算式(&self, 游標: usize) -> Option<(Ｏ算式, usize)> {
-        let (mut 算式, mut 游標) = self.剖析乘除式(游標)?;
-        while let Some((運算子, 新游標)) = self.消耗加減(游標) {
-            let (右算元, 新游標) = self.剖析乘除式(新游標)?;
+        let (原子式, mut 游標) = self.剖析原子式(游標)?;
 
-            算式 = Ｏ算式::二元運算(Ｏ二元運算 {
-                左: Box::new(算式),
-                右: Box::new(右算元),
-                運算子,
-            });
+        // TODO: 將算子棧算元棧包裝到一個 struct 裡
+        let mut 算元棧 = VecDeque::<Ｏ算式>::new();
+        算元棧.push_back(原子式);
+
+        let mut 算子棧 = VecDeque::<Ｏ運算子>::new();
+
+        while let Some((新算子, 新游標)) = self.消耗運算子(游標) {
+            let (新算元, 新游標) = self.剖析原子式(新游標)?;
+
+            // 讀取到新算子，進行棧操作
+            while !算子棧.is_empty() && 優先級(算子棧.back().unwrap()) >= 優先級(&新算子)
+            {
+                // 新算子優先級較低，代表棧中的算子算元可以先結合了。
+                let 右算元 = 算元棧.pop_back().unwrap();
+                let 左算元 = 算元棧.pop_back().unwrap();
+                let 運算子 = 算子棧.pop_back().unwrap();
+                算元棧.push_back(Ｏ算式::二元運算(Ｏ二元運算 {
+                    運算子,
+                    左: Box::new(左算元),
+                    右: Box::new(右算元),
+                }));
+            }
+
+            // 原式中能決定結合的算子跟算元都決定了，推入新算子跟算元
+            算子棧.push_back(新算子);
+            算元棧.push_back(新算元);
+
             游標 = 新游標
         }
-        Some((算式, 游標))
-    }
 
-    fn 剖析乘除式(&self, 游標: usize) -> Option<(Ｏ算式, usize)> {
-        let (mut 算式, mut 游標) = self.剖析原子式(游標)?;
-        while let Some((運算子, 新游標)) = self.消耗乘除(游標) {
-            let (右算元, 新游標) = self.剖析原子式(新游標)?;
-
-            算式 = Ｏ算式::二元運算(Ｏ二元運算 {
-                左: Box::new(算式),
-                右: Box::new(右算元),
+        while !算子棧.is_empty() {
+            // 無新算子，棧中的算子算元最右向左依次結合
+            // TODO: 封裝此二相同 while 內容
+            let 右算元 = 算元棧.pop_back().unwrap();
+            let 左算元 = 算元棧.pop_back().unwrap();
+            let 運算子 = 算子棧.pop_back().unwrap();
+            算元棧.push_back(Ｏ算式::二元運算(Ｏ二元運算 {
                 運算子,
-            });
-            游標 = 新游標
+                左: Box::new(左算元),
+                右: Box::new(右算元),
+            }));
         }
-        Some((算式, 游標))
+
+        assert_eq!(算元棧.len(), 1);
+
+        Some((算元棧.pop_back().unwrap(), 游標))
     }
 
     fn 剖析原子式(&self, 游標: usize) -> Option<(Ｏ算式, usize)> {
@@ -133,9 +173,9 @@ impl Ｏ剖析器 {
         }
         // 原子式 = （算式）
         if let Some(結果) = (|| -> Option<(Ｏ算式, usize)> {
-            let 游標 = self.消耗(游標, Ｏ詞::左括號)?;
+            let 游標 = self.消耗(游標, Ｏ詞::左圓括號)?;
             let (算式, 游標) = self.剖析算式(游標)?;
-            let 游標 = self.消耗(游標, Ｏ詞::右括號)?;
+            let 游標 = self.消耗(游標, Ｏ詞::右圓括號)?;
             Some((算式, 游標))
         })() {
             return Some(結果);
@@ -153,29 +193,16 @@ impl Ｏ剖析器 {
 
     fn 剖析變數(&self, 游標: usize) -> Option<(String, usize)> {
         match self.詞組.get(游標) {
-            Some(Ｏ詞::變數(變數名)) => Some((變數名.clone(), 游標 + 1)),
+            Some(Ｏ詞::識別子(變數名)) => Some((變數名.clone(), 游標 + 1)),
             Some(_) => None,
             _ => None,
         }
     }
 
-    fn 消耗乘除(&self, 游標: usize) -> Option<(Ｏ運算子, usize)> {
-        if let Some(游標) = self.消耗(游標, Ｏ詞::運算子(Ｏ運算子::乘)) {
-            Some((Ｏ運算子::乘, 游標))
-        } else if let Some(游標) = self.消耗(游標, Ｏ詞::運算子(Ｏ運算子::除)) {
-            Some((Ｏ運算子::除, 游標))
-        } else {
-            None
-        }
-    }
-
-    fn 消耗加減(&self, 游標: usize) -> Option<(Ｏ運算子, usize)> {
-        if let Some(游標) = self.消耗(游標, Ｏ詞::運算子(Ｏ運算子::加)) {
-            Some((Ｏ運算子::加, 游標))
-        } else if let Some(游標) = self.消耗(游標, Ｏ詞::運算子(Ｏ運算子::減)) {
-            Some((Ｏ運算子::減, 游標))
-        } else {
-            None
+    fn 消耗運算子(&self, 游標: usize) -> Option<(Ｏ運算子, usize)> {
+        match self.詞組.get(游標) {
+            Some(Ｏ詞::運算子(運算子)) => Some((運算子.clone(), 游標 + 1)),
+            _ => None,
         }
     }
 
