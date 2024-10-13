4bdf00bdd6028256b81b70d5dfdc833b45b6ba52
MROS <yc1043@gmail.com>
2024-10-13 21:23:58 +0800

    剖析術宣告

--- 零號編譯器/src/剖析/抽象語法樹節點.rs
+++ 零號編譯器/src/剖析/抽象語法樹節點.rs
@@ -5,13 +5,20 @@ pub type Ｏ語法樹 = Ｏ咒;
 
 #[derive(Debug)]
 pub struct Ｏ咒 {
-    pub 句: Vec<Ｏ句>,
+    pub 頂層宣告: Vec<Ｏ頂層宣告>,
 }
 
-#[derive(Debug, From)]
+#[derive(Debug, PartialEq, From)]
+pub enum Ｏ頂層宣告 {
+    變數宣告(Ｏ變數宣告),
+    術宣告(Ｏ術宣告),
+}
+
+#[derive(Debug, PartialEq, From)]
 pub enum Ｏ句 {
     變數宣告(Ｏ變數宣告),
     算式(Ｏ算式),
+    歸(Ｏ歸),
 }
 
 #[derive(Debug, PartialEq)]
@@ -25,6 +32,23 @@ pub enum Ｏ算式 {
     變數(String),
     數字(i64),
     二元運算(Ｏ二元運算),
+    施術(Ｏ施術),
+}
+
+#[derive(Debug, PartialEq, From)]
+pub struct Ｏ歸(pub Ｏ算式);
+
+#[derive(Debug, PartialEq)]
+pub struct Ｏ術宣告 {
+    pub 術名: String,
+    pub 形參: Vec<String>,
+    pub 術體: Vec<Ｏ句>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Ｏ施術 {
+    pub 術名: String,
+    pub 實參: Vec<Ｏ算式>,
 }
 
 #[derive(Debug, PartialEq)]
--- 零號編譯器/src/剖析/組合子.rs
+++ 零號編譯器/src/剖析/組合子.rs
@@ -1,9 +1,23 @@
 use crate::分詞器::Ｏ詞;
 
 pub trait 組合子<'a, T, U> {
+    // 接受一個返回 (剖析結果, &[Ｏ詞]) 的函式
+    // 把剖析結果包進 tuple 裡傳遞給下個組合子
     fn 且<F>(self, f: F) -> Option<((T, U), &'a [Ｏ詞])>
     where
         F: FnOnce(&'a [Ｏ詞]) -> Option<(U, &'a [Ｏ詞])>;
+
+    // 接受一個只返回 (剖析結果, &[Ｏ詞]) 的函式
+    // 但會把剖析結果直接拋棄
+    // 以免浪費空間、增加 tuple 層級
+    fn 且棄<F>(self, f: F) -> Option<(T, &'a [Ｏ詞])>
+    where
+        F: FnOnce(&'a [Ｏ詞]) -> Option<(U, &'a [Ｏ詞])>;
+
+    // 重複 0 到多次
+    fn 重複<F>(self, f: F) -> Option<((T, Vec<U>), &'a [Ｏ詞])>
+    where
+        F: Fn(&'a [Ｏ詞]) -> Option<(U, &'a [Ｏ詞])>;
 
     fn 映射<F>(self, f: F) -> Option<(U, &'a [Ｏ詞])>
     where
@@ -19,6 +33,31 @@ impl<'a, T, U> 組合子<'a, T, U> for Option<(T, &'a [Ｏ詞])> {
             // 將之前的剖析結果與新剖析結果以 tuple 包裝
             f(游標).map(|(新剖析結果, 游標)| ((剖析結果, 新剖析結果), 游標))
         })
+    }
+
+    fn 且棄<F>(self, f: F) -> Option<(T, &'a [Ｏ詞])>
+    where
+        F: FnOnce(&'a [Ｏ詞]) -> Option<(U, &'a [Ｏ詞])>,
+    {
+        self.and_then(|(剖析結果, 游標)| f(游標).map(|(_, 游標)| (剖析結果, 游標)))
+    }
+
+    fn 重複<F>(self, f: F) -> Option<((T, Vec<U>), &'a [Ｏ詞])>
+    where
+        F: Fn(&'a [Ｏ詞]) -> Option<(U, &'a [Ｏ詞])>,
+    {
+        match self {
+            Some((t, 詞組)) => {
+                let mut 當前詞組 = 詞組;
+                let mut 剖析結果陣列 = Vec::new();
+                while let Some((剖析結果, 詞組)) = f(當前詞組) {
+                    當前詞組 = 詞組;
+                    剖析結果陣列.push(剖析結果);
+                }
+                Some(((t, 剖析結果陣列), 當前詞組))
+            }
+            None => None,
+        }
     }
 
     fn 映射<F>(self, f: F) -> Option<(U, &'a [Ｏ詞])>
