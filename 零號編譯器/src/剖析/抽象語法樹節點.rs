f3fad508af3a658f2290a6c8777ba948d0a11a3d
MROS <yc1043@gmail.com>
2024-10-13 16:55:59 +0800

    加入 derive_more 以自動生成 enum 轉換

--- 零號編譯器/src/剖析/抽象語法樹節點.rs
+++ 零號編譯器/src/剖析/抽象語法樹節點.rs
@@ -1,6 +1,5 @@
-use super::組合子::{組合子, 自動映射組合子};
-use super::調車場::Ｏ調車場;
-use crate::分詞器::{Ｏ分詞器, Ｏ詞, Ｏ運算子};
+use crate::分詞器::Ｏ運算子;
+use derive_more::From;
 
 pub type Ｏ語法樹 = Ｏ咒;
 
@@ -9,20 +8,10 @@ pub struct Ｏ咒 {
     pub 句: Vec<Ｏ句>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, From)]
 pub enum Ｏ句 {
     變數宣告(Ｏ變數宣告),
     算式(Ｏ算式),
-}
-impl Into<Ｏ句> for Ｏ變數宣告 {
-    fn into(self) -> Ｏ句 {
-        Ｏ句::變數宣告(self)
-    }
-}
-impl Into<Ｏ句> for Ｏ算式 {
-    fn into(self) -> Ｏ句 {
-        Ｏ句::算式(self)
-    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -31,26 +20,11 @@ pub struct Ｏ變數宣告 {
     pub 算式: Ｏ算式,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, From)]
 pub enum Ｏ算式 {
     變數(String),
     數字(i64),
     二元運算(Ｏ二元運算),
-}
-impl Into<Ｏ算式> for String {
-    fn into(self) -> Ｏ算式 {
-        Ｏ算式::變數(self)
-    }
-}
-impl Into<Ｏ算式> for i64 {
-    fn into(self) -> Ｏ算式 {
-        Ｏ算式::數字(self)
-    }
-}
-impl Into<Ｏ算式> for Ｏ二元運算 {
-    fn into(self) -> Ｏ算式 {
-        Ｏ算式::二元運算(self)
-    }
 }
 
 #[derive(Debug, PartialEq)]
