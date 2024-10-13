507eabfdcab46f0bc0205db77cd5387c8339509f
MROS <yc1043@gmail.com>
2024-10-13 16:13:34 +0800

    將語法樹節點置於獨立檔案

--- 零號編譯器/src/剖析/剖析器.rs
+++ 零號編譯器/src/剖析/剖析器.rs
@@ -1,53 +1,7 @@
+use super::抽象語法樹節點::*;
 use super::組合子::{組合子, 自動映射組合子};
 use super::調車場::Ｏ調車場;
 use crate::分詞器::{Ｏ分詞器, Ｏ詞, Ｏ運算子};
-
-pub struct Ｏ剖析器 {
-    詞組: Vec<Ｏ詞>,
-}
-
-pub type Ｏ語法樹 = Ｏ咒;
-
-#[derive(Debug)]
-pub struct Ｏ咒 {
-    pub 句: Vec<Ｏ句>,
-}
-
-#[derive(Debug)]
-pub enum Ｏ句 {
-    變數宣告(Ｏ變數宣告),
-    算式(Ｏ算式),
-}
-impl Into<Ｏ句> for Ｏ變數宣告 {
-    fn into(self) -> Ｏ句 {
-        Ｏ句::變數宣告(self)
-    }
-}
-impl Into<Ｏ句> for Ｏ算式 {
-    fn into(self) -> Ｏ句 {
-        Ｏ句::算式(self)
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Ｏ變數宣告 {
-    pub 變數名: String,
-    pub 算式: Ｏ算式,
-}
-
-#[derive(Debug, PartialEq)]
-pub enum Ｏ算式 {
-    變數(String),
-    數字(i64),
-    二元運算(Ｏ二元運算),
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Ｏ二元運算 {
-    pub 運算子: Ｏ運算子,
-    pub 左: Box<Ｏ算式>,
-    pub 右: Box<Ｏ算式>,
-}
 
 fn 剖析咒(詞組: &[Ｏ詞]) -> Option<(Ｏ咒, &[Ｏ詞])> {
     let mut 咒 = Ｏ咒 { 句: Vec::new() };
@@ -165,9 +119,13 @@ fn 消耗(詞: Ｏ詞, 詞組: &[Ｏ詞]) -> Option<((), &[Ｏ詞])> {
     }
 }
 
+pub struct Ｏ剖析器 {
+    詞組: Vec<Ｏ詞>,
+}
+
 impl Ｏ剖析器 {
-    pub fn new(詞流: Vec<Ｏ詞>) -> Self {
-        Ｏ剖析器 { 詞組: 詞流 }
+    pub fn new(詞組: Vec<Ｏ詞>) -> Self {
+        Ｏ剖析器 { 詞組 }
     }
 
     pub fn 剖析(&mut self) -> Option<Ｏ語法樹> {
---
+++ 零號編譯器/src/剖析/抽象語法樹節點.rs
@@ -0,0 +1,46 @@
+use super::組合子::{組合子, 自動映射組合子};
+use super::調車場::Ｏ調車場;
+use crate::分詞器::{Ｏ分詞器, Ｏ詞, Ｏ運算子};
+
+pub type Ｏ語法樹 = Ｏ咒;
+
+#[derive(Debug)]
+pub struct Ｏ咒 {
+    pub 句: Vec<Ｏ句>,
+}
+
+#[derive(Debug)]
+pub enum Ｏ句 {
+    變數宣告(Ｏ變數宣告),
+    算式(Ｏ算式),
+}
+impl Into<Ｏ句> for Ｏ變數宣告 {
+    fn into(self) -> Ｏ句 {
+        Ｏ句::變數宣告(self)
+    }
+}
+impl Into<Ｏ句> for Ｏ算式 {
+    fn into(self) -> Ｏ句 {
+        Ｏ句::算式(self)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Ｏ變數宣告 {
+    pub 變數名: String,
+    pub 算式: Ｏ算式,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Ｏ算式 {
+    變數(String),
+    數字(i64),
+    二元運算(Ｏ二元運算),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Ｏ二元運算 {
+    pub 運算子: Ｏ運算子,
+    pub 左: Box<Ｏ算式>,
+    pub 右: Box<Ｏ算式>,
+}
